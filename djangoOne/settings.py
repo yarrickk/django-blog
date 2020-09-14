338b348d546dcd7f099a1cb770a1e62e1252395e
yarrickk <yaroslav.babych@gmail.com>
2020-09-13 18:52:02 -0700

    Added Login / Logout

--- djangoOne/settings.py
+++ djangoOne/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'djangoOne.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +118,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
