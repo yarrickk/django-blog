157875761bfe9e0b8bcc185f8b30de2b7a1c1ccf
yarrickk <yaroslav.babych@gmail.com>
2020-09-13 20:31:27 -0700

    Added Profiles with Images

--- djangoOne/urls.py
+++ djangoOne/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from users import views as users_views
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +29,6 @@ urlpatterns = [
     path('profile/', users_views.profile, name='profile'),
     path('', include("blog.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
