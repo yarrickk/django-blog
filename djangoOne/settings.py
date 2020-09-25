b2f8efaf48a3f9d36e0c4abcc272413456db7c80
yarrickk <yaroslav.babych@gmail.com>
2020-09-24 21:46:51 -0700

    Password reset done

--- djangoOne/settings.py
+++ djangoOne/settings.py
@@ -127,3 +127,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIl_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "username"
+EMAIL_HOST_PASSWORD = "password"
+
